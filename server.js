0403979e83a5cedbd300c9f670679bf7a12885db
Caio Vinícius Piologo Véras Fernandes <caiovpiologo@gmail.com>
2018-04-06 00:38:04 -0300

    mongodb integrated

--- server.js
+++ server.js
@@ -5,13 +5,23 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
+const MongoClient = require('mongodb').MongoClient
+
+var db;
+MongoClient.connect('mongodb://mc851_payment:mc8512018@ds143362.mlab.com:43362/mc851_payment_service',
+ (err, client) => {
+   if (err) return console.log(err);
+   db = client.db('mc851_payment_service');
+   app.listen(port, () => console.log(`Listening on port ${port}`));
+})
+
 /* Uncomment this line to use the server with the react application */
 // app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello World' });
+   res.send({ express: 'Hello World' });
 });
 
 app.post('/payments/creditCard', (req, res) => {
@@ -26,4 +36,14 @@ app.post('/payments/creditCard', (req, res) => {
    res.send({card_name: name});
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.post('/quotes', (req, res) => {
+
+   db.collection('quotes').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+
+      console.log('saved to database')
+      res.redirect('/')
+   })
+})
+
+// app.listen(port, () => console.log(`Listening on port ${port}`));
