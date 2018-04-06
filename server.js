12a045c6125dc887d6856539569cc97d9ce01019
Caio Vinícius Piologo Véras Fernandes <caiovpiologo@gmail.com>
2018-04-05 23:30:26 -0300

    added body-parser

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
 /* Uncomment this line to use the server with the react application */
 // app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello World' });
@@ -13,13 +15,14 @@ app.get('/api/hello', (req, res) => {
 
 app.post('/payments/creditCard', (req, res) => {
    //receive stuff from require (req)
-   var name = req.param('name');
+   // var name = req.body;
+   console.log(req.body);
 
 
    //do stuff with it (buy with some api)
 
    //then return any response if needed
-   res.send({card_name: name});
+   // res.send({card_name: name});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
