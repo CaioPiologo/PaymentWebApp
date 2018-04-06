2c1d1a9f7e01d0f91d72c3008ae8142ea939bf0c
Felipe Dal Mas Eulalio <feulalio@apple.com>
2018-04-06 19:27:23 -0300

    Added invoice endpoint

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
-const bodyParser = require('body-parser');
-const port = process.env.PORT || 5000;
+const app = express()
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 5000
 
 const MongoClient = require('mongodb').MongoClient
 
-var db;
+var db
 MongoClient.connect('mongodb://mc851_payment:mc8512018@ds143362.mlab.com:43362/mc851_payment_service',
  (err, client) => {
-   if (err) return console.log(err);
-   db = client.db('mc851_payment_service');
-   app.listen(port, () => console.log(`Listening on port ${port}`));
-})
+   if (err) return console.log(err)
+   db = client.db('mc851_payment_service')
+   app.listen(port, () => console.log(`Listening on port ${port}`))
+ })
 
 /* Uncomment this line to use the server with the react application */
 // app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/api/hello', (req, res) => {
-   res.send({ express: 'Hello World' });
-});
+  res.send({ express: 'Hello World' })
+})
 
 app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
 
-   db.collection('quotes').save(req.body, (err, result) => {
-      if (err) return console.log(err)
-
-      console.log('saved to database')
-      res.redirect('/')
-   })
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
 
 /// Check the informations, (in the future contact the credit module) and return the payment status
@@ -54,12 +53,15 @@ app.post('/payments/creditCard', (req, res) => {
       !clientInfo.hasOwnProperty('value') ||
       !clientInfo.hasOwnProperty('instalments')) {
     result = 'UNAUTHORIZED'
+    responseCode = 400
     errorMessage = 'Missing information.'
   } else if (clientInfo.cpf.toString().length !== 11) {
     result = 'UNAUTHORIZED'
+    responseCode = 400
     errorMessage = 'Wrong CPF.'
   } else if (clientInfo.cardNumber.toString().length !== 16) {
     result = 'UNAUTHORIZED'
+    responseCode = 400
     errorMessage = 'Wrong card number.'
   } else {
     opHash = Math.random().toString(36).substring(2)
@@ -70,8 +72,8 @@ app.post('/payments/creditCard', (req, res) => {
     operationHash: opHash,
     result: result,
     errorMessage: errorMessage
-  });
-});
+  })
+})
 
 /// Return the possible instalments values given the card flag.
 app.post('/payments/creditCard/installments', (req, res) => {
@@ -115,10 +117,13 @@ app.post('/payments/bankTicket', (req, res) => {
       !clientInfo.hasOwnProperty('address') ||
       !clientInfo.hasOwnProperty('cep') ||
       !clientInfo.hasOwnProperty('value')) {
+    responseCode = 400
     errorMessage = 'Missing information.'
   } else if (clientInfo.cpf.toString().length !== 11) {
+    responseCode = 400
     errorMessage = 'Wrong CPF.'
   } else if (clientInfo.cep.toString().length !== 8) {
+    responseCode = 400
     errorMessage = 'Wrong CEP.'
   } else {
     code = Math.random().toString(36).substring(2)
@@ -162,5 +167,41 @@ app.get('/payments/bankTicket/:code/status', (req, res) => {
 
   res.status(responseCode).send({
     status: status
+  })
+})
+
+app.post('/invoice', (req, res) => {
+  var body = req.body
+  console.log(body)
+
+  var code = null
+  var errorMessage = null
+  var responseCode = 200
+
+  if (!body.hasOwnProperty('clientData') ||
+      !body.hasOwnProperty('products') ||
+      !body.hasOwnProperty('paymentMethod') ||
+      !body.hasOwnProperty('value') ||
+      !body.hasOwnProperty('transportValue') ||
+      !body.hasOwnProperty('discountValue') ||
+      !body.hasOwnProperty('totalValue')) {
+    responseCode = 400
+    errorMessage = 'Missing information.'
+  } else if (body.clientData.cpf.toString().length !== 11) {
+    responseCode = 400
+    errorMessage = 'Wrong CPF.'
+  } else if (body.clientData.address.cep.toString().length !== 8) {
+    responseCode = 400
+    errorMessage = 'Wrong CEP.'
+  } else {
+    code = Math.random().toString(36).substring(2)
+  }
+
+  // TODO: Generate document and add it status to the DB
+
+  res.status(responseCode).send({
+    invoiceCode: code,
+    errorMessage: errorMessage,
+    documentRep: ''
   })
 })
